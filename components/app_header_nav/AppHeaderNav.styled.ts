c425d60239f71efd3b8013b338fbbd8fe7734457
Tunde Alabi <tundealabi4780@gmail.com>
2021-10-22 16:35:38 +0100

    feat(pages/index.tsx): implement the landing page
    implement the landing page and make it mobile responsive, including the header nav and the main
    content area
    
    fix #4

---
+++ components/app_header_nav/AppHeaderNav.styled.ts
@@ -0,0 +1,32 @@
+import AppBar from '@mui/material/AppBar';
+import ToolBar from '@mui/material/Toolbar';
+import styled from 'styled-components';
+
+const AppHeaderNavContainer = styled(AppBar)``;
+
+const AppHeaderNavToolBar = styled(ToolBar)`
+  justify-content: space-between;
+  /* fallback for old browsers */
+  background: #abbaab;
+  /* for Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-linear-gradient(to right, #ffffff, #abbaab);
+  /* for W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: linear-gradient(to right, #ffffff, #abbaab);
+  padding: 0 2rem;
+  box-shadow: 0px 1px 2px 1px rgba(134, 134, 134, 0.1);
+  @media (min-width: 710px) {
+    padding: 0 9.3rem;
+  }
+`;
+
+const AppHeaderNavLogoContainer = styled.div`
+  /* border: 1px solid yellow; */
+  cursor: pointer;
+  /* margin-left: 4rem; */
+  flex-basis: 15%;
+  @media (max-width: 500px) {
+    flex-basis: 28%;
+  }
+`;
+
+export { AppHeaderNavContainer, AppHeaderNavToolBar, AppHeaderNavLogoContainer };
---
+++ styles/Landing.styled.ts
@@ -0,0 +1,72 @@
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import styled from 'styled-components';
+
+const LandingPageContainer = styled.div`
+  min-height: 100vh;
+  padding: 0 2.5rem;
+  /* display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center; */
+  height: 100vh;
+  /* fallback for old browsers */
+  background: #abbaab;
+  /* for Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-linear-gradient(to right, #ffffff, #abbaab);
+  /* for W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: linear-gradient(to right, #ffffff, #abbaab);
+
+  @media (min-width: 710px) {
+    padding: 0 9.3rem;
+  }
+`;
+
+const LandingPageMainContentContainer = styled.div<{ showbackgroundimage: boolean }>`
+  background-image: url(${(props) => (props.showbackgroundimage ? '/banner-image.png' : '')});
+  background-image: '/banner-image.png';
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  /* border: 1px solid yellow; */
+`;
+const LandingPageMainDescriptionContainer = styled.div`
+  /* border: 1px solid green; */
+  @media (min-width: 900px) {
+    width: 58%;
+  }
+`;
+const LandingPageMainDescriptionHeading = styled(Typography)`
+  font-size: 4rem;
+  line-height: 5.937rem;
+  margin-bottom: 1rem;
+  font-weight: 700;
+`;
+const LandingPageDescriptionSubtitle = styled(Typography)`
+  font-size: 1.312rem;
+  font-weight: 500;
+  font-family: 'Courier New', Courier, monospace;
+  line-height: 2.25rem;
+  margin-bottom: 2rem;
+`;
+
+const LandingPageAuthButton = styled(Button)`
+  background-color: #2dd882;
+  box-shadow: 0 8px 10px rgba(45, 216, 130, 0.3);
+
+  :hover {
+    background-color: #2dd882;
+  }
+`;
+
+export {
+  LandingPageContainer,
+  LandingPageMainContentContainer,
+  LandingPageMainDescriptionContainer,
+  LandingPageMainDescriptionHeading,
+  LandingPageDescriptionSubtitle,
+  LandingPageAuthButton,
+};
